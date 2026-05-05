dd8f29947624e3ca2aa0f4df8eaba60a4fe4a0c7
Mariem Bouaziz <mariembouaziz.mb@gmail.com>
2026-05-05 13:12:48 +0200

    ajout data cleaning scripts

--- data_cleaning/cleanMytek.py
+++ data_cleaning/cleanMytek.py
@@ -79,24 +79,15 @@ def clean_price(price):
 # -----------------------------
 # Valider prix
 # -----------------------------
-def clean_prices(price_final, price_original):
-    pf = clean_price(price_final)
-    po = clean_price(price_original)
-
-    # ❌ Si prix final invalide → produit ignoré
-    if pf is None or pf < 10:
-        return None, None
-
-    # 🟢 CAS 1 : pas de prix original → pas de promo
-    if po is None:
-        po = pf
-
-    # 🟡 CAS 2 : incohérence
-    elif po < pf:
-        po = pf
-
-    # 🟢 CAS 3 : promo valide → on garde
-
+def clean_prices(price_final, price_original): 
+    pf = clean_price(price_final) 
+    po = clean_price(price_original) 
+    # price_original invalide si <= price_final 
+    if po is not None and pf is not None and po <= pf: 
+        po = None 
+    # prix aberrant 
+    if pf is not None and pf < 10: 
+        pf = None 
     return pf, po
 
 # -----------------------------
